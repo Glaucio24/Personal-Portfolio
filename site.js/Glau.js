dfb6fc19d4e08ecc67912c6e04f9a54bba85e279
Glaucio Alexandre <108555972+Glaucio24@users.noreply.github.com>
2023-03-16 17:09:10 -0500

    Init commit

---
+++ site.js/Glau.js
@@ -0,0 +1,26 @@
+const navSlider=()=>{
+    const burger=document.querySelector('.burger');
+    const navLinks=document.querySelector('.links');
+    const navList=document.querySelectorAll('.links li');
+
+     burger.addEventListener('click', () => {
+        //toggle nav
+     navLinks.classList.toggle('nav-active');
+        //Animate 
+     navList.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation='';
+        }
+        else{
+        link.style.animation=`navLinkFade 0.5s ease forwards ${index / 7+0.02}s`;
+        }   
+     });
+      //burger Animation
+      burger.classList.toggle('atoggle');
+  });
+}
+const app=()=>{
+    navSlider();
+   
+}
+app();
